39d02d69de0472902a2b406148c6893804743cc2
Gavin Veitch <gavintveitch@gmail.com>
2022-11-09 10:28:25 -0700

    commit tree 916a9e46b087eb981089d680d377443ebd4042e6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,66 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+  end
+
+  def kind
+    if (self.isTriangle == true)
+      if ( @side1 == @side2)
+        if ( @side1 == @side3 )
+          if ( @side2 == @side3 )
+            return :equilateral
+          end
+        end
+      end
+
+      if ( @side1 == @side2 )
+        return :isosceles
+      end
+      if ( @side1 == @side3 )
+        return :isosceles
+      end
+      if ( @side2 == @side3 )
+        return :isosceles
+      end
+
+      :scalene
+
+    else
+      begin
+        raise TriangleError
+      end
+
+    end
+  end
+
+  def isTriangle
+    if (@side1 > 0)
+      if( @side2 > 0)
+        if (@side3 > 0)
+          if (@side1 < @side2 + @side3)
+            if (@side2 < @side1 + @side3)
+              if (@side3 < @side1 + @side2)
+                return true
+              end
+            end
+          end
+        end
+      end
+    end
+    return false
+  end
+
+  class TriangleError < StandardError
+    def message
+      "????? thats not a triangle"
+    end
+  end
+
 end
